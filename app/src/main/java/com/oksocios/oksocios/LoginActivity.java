4ebca6660dbb62c54f5e5a3903ac8935a1aa9e9f
Nicolas Rios <riosc.nicolas@gmail.com>
2017-03-10 21:21:01 -0300

    Swipe Views start

--- app/src/main/java/com/oksocios/oksocios/LoginActivity.java
+++ app/src/main/java/com/oksocios/oksocios/LoginActivity.java
@@ -1,7 +1,10 @@
 package com.oksocios.oksocios;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -11,11 +14,17 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
     }
 
-    public void signIn(){
-
+    public void signIn(View view){
+        Toast.makeText(getApplicationContext(),"Implementar funcion signIn",Toast.LENGTH_SHORT).show();
     }
 
-    public void forgotPassword(){
-
+    public void forgotPassword(View view){
+        Toast.makeText(getApplicationContext(),"Implementar funcion forgotPassword",Toast.LENGTH_SHORT).show();
     }
+
+    public void logIn(View view){
+        Toast.makeText(getApplicationContext(),"Implementar funcion logIn()",Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent(this,ContainerActivity.class);
+        startActivity(intent);
+}
 }
